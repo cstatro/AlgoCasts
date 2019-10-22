a8ff4c4ba803c2e2bb33fc6b76ae9cf1d7e0225e
Colin <cstatro@gmail.com>
2019-10-21 20:31:17 -0400

    done with max char

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,21 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  const strObj = str.split("").reduce((a, c) => {
+    a[c] ? (a[c] += 1) : (a[c] = 1);
+    return a;
+  }, {});
+
+  maxCount = 0;
+  maxLetter = "";
+  for (key in strObj) {
+    if (strObj[key] > maxCount) {
+      maxCount = strObj[key];
+      maxLetter = key;
+    }
+  }
+  return maxLetter;
+}
 
 module.exports = maxChar;
