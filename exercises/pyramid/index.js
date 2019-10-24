d829f338df379dc7ef82e7eacf8d14b7689caf39
Colin <cstatro@gmail.com>
2019-10-24 12:13:40 -0700

    passing pyramid test

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,43 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+function pyramid(n) {
+  const columns = n * 2 - 1;
+  const mid = Math.floor(columns / 2);
+  for (let row = 0; row < n; row++) {
+    let step = "";
+    for (let column = 0; column < columns; column++) {
+      if (mid - row <= column && mid + row >= column) {
+        step += "#";
+      } else {
+        step += " ";
+      }
+    }
+    console.log(step);
+  }
+}
+
+// function pyramid(n) {
+//   // row size is n + (n-1)
+//   //center position  of row
+//   // stop when pyramid row equals n
+//   const totalColumns = n + (n - 1);
+//   const center = Math.ceil(totalColumns / 2);
+//   const offSet = num => Math.floor(num / 2);
+
+//   let row = 1;
+//   while (row <= n) {
+//     let step = "";
+//     let currentOffset = offSet(row);
+//     for (let i = 1; i <= totalColumns; i++) {
+//       step +=
+//         i >= center - currentOffset && i < center + currentOffset + 1
+//           ? "#"
+//           : " ";
+//     }
+//     row += 1;
+//     console.log(step);
+//   }
+// }
 
 module.exports = pyramid;
