df41a077a453f679a45bbd3f47dda445df11b939
Colin <cstatro@gmail.com>
2019-10-24 12:26:03 -0700

    ok

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,10 +14,12 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {
-  const columns = n * 2 - 1;
-  const mid = Math.floor(columns / 2);
-  for (let row = 0; row < n; row++) {
+function pyramid(n, row = 0) {
+  if (row === n) {
+    return;
+  } else {
+    const columns = n * 2 - 1;
+    const mid = Math.floor(columns / 2);
     let step = "";
     for (let column = 0; column < columns; column++) {
       if (mid - row <= column && mid + row >= column) {
@@ -27,28 +29,22 @@ function pyramid(n) {
       }
     }
     console.log(step);
+    return pyramid(n, row + 1);
   }
 }
 
 // function pyramid(n) {
-//   // row size is n + (n-1)
-//   //center position  of row
-//   // stop when pyramid row equals n
-//   const totalColumns = n + (n - 1);
-//   const center = Math.ceil(totalColumns / 2);
-//   const offSet = num => Math.floor(num / 2);
-
-//   let row = 1;
-//   while (row <= n) {
+//   const columns = n * 2 - 1;
+//   const mid = Math.floor(columns / 2);
+//   for (let row = 0; row < n; row++) {
 //     let step = "";
-//     let currentOffset = offSet(row);
-//     for (let i = 1; i <= totalColumns; i++) {
-//       step +=
-//         i >= center - currentOffset && i < center + currentOffset + 1
-//           ? "#"
-//           : " ";
+//     for (let column = 0; column < columns; column++) {
+//       if (mid - row <= column && mid + row >= column) {
+//         step += "#";
+//       } else {
+//         step += " ";
+//       }
 //     }
-//     row += 1;
 //     console.log(step);
 //   }
 // }
