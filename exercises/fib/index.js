8094fef375686572394a69bf6fc16896dc180b02
Colin <cstatro@gmail.com>
2019-10-28 18:16:31 -0400

    fib done

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,16 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+function fib(n, arr = []) {
+  if (arr.length === 0) {
+    const newArr = [1, 1];
+    return fib(n, newArr);
+  } else if (arr.length >= n) {
+    return arr[n - 1];
+  } else {
+    const newArr = [...arr, arr[arr.length - 1] + arr[arr.length - 2]];
+    return fib(n, newArr);
+  }
+}
 
 module.exports = fib;
